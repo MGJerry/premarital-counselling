6794a2170fdefa10de5ec3aabf054e5e3b322d77
cntduongg <cntduongg.9@gmail.com>
2025-02-28 15:00:44 +0700

    Regis and Login

---
+++ src/main/java/com/example/demo/service/AuthenticationService.java
@@ -0,0 +1,75 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.User;
+import com.example.demo.entity.request.AuthenticationRequest;
+import com.example.demo.entity.request.UserRequest;
+import com.example.demo.entity.response.AuthenticationResponse;
+import com.example.demo.respository.AuthenticationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+    @Autowired
+    AuthenticationRepository authenticationRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    TokenService tokenService;
+
+    public User register(UserRequest userRequest){
+
+
+        User user = new User();
+
+        user.setUsername(userRequest.getUsername());
+        user.setPassword(passwordEncoder.encode(userRequest.getPassword()));
+        user.setFullName(userRequest.getFullName());
+        user.setPhone(userRequest.getPhone());
+        user.setEmail(userRequest.getEmail());
+
+        User newAccount = authenticationRepository.save(user);
+        return newAccount;
+    }
+
+
+
+
+
+    public AuthenticationResponse login(AuthenticationRequest authenticationRequest) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            authenticationRequest.getUsername(),
+                            authenticationRequest.getPassword()
+                    )
+            );
+        } catch (Exception e) {
+            throw new NullPointerException("Wrong email or password");
+        }
+        User user = authenticationRepository.findByUsername(authenticationRequest.getUsername()).orElseThrow();
+        String token = tokenService.generateToken(user);
+
+        AuthenticationResponse authenticationResponse = new AuthenticationResponse();
+        authenticationResponse.setEmail(user.getEmail());
+        authenticationResponse.setId(user.getId());
+        authenticationResponse.setFullName(user.getFullName());
+        authenticationResponse.setUsername(user.getUsername());
+        authenticationResponse.setToken(token);
+
+        return authenticationResponse;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return (UserDetails) authenticationRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Account not found"));
+    }
+}

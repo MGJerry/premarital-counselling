532d5c34ef2e99557c25f37f8b4e60d07e25dc09
cntduongg <cntduongg.9@gmail.com>
2025-03-10 02:21:44 +0700

    update profile

--- src/main/java/com/example/demo/api/UserAPI.java
+++ src/main/java/com/example/demo/api/UserAPI.java
@@ -2,8 +2,10 @@ package com.example.demo.api;
 
 import com.example.demo.entity.User;
 import com.example.demo.entity.request.AuthenticationRequest;
+import com.example.demo.entity.request.UpdateRequest;
 import com.example.demo.entity.request.UserRegisterRequest;
 import com.example.demo.entity.response.AuthenticationResponse;
+import com.example.demo.entity.response.UpdateResponse;
 import com.example.demo.service.AuthenticationService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,5 +31,10 @@ public class UserAPI {
         return ResponseEntity.ok(authenticationResponse);
     }
 
+    @PutMapping("update")
+    public ResponseEntity update(@RequestBody UpdateRequest updateRequest){
+        UpdateResponse updateResponse = authenticationService.updateProfile(updateRequest);
+        return ResponseEntity.ok(updateResponse);
+    }
     
 }

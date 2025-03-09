c23fa549036b2d84f040897e497847c24e6741fd
cntduongg <cntduongg.9@gmail.com>
2025-03-10 02:49:42 +0700

    add validation in login, register

--- src/main/java/com/example/demo/entity/request/AuthenticationRequest.java
+++ src/main/java/com/example/demo/entity/request/AuthenticationRequest.java
@@ -1,8 +1,15 @@
 package com.example.demo.entity.request;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class AuthenticationRequest {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     String email;
+
+    @NotBlank(message = "Password is required")
     String password;
 
     public AuthenticationRequest() {

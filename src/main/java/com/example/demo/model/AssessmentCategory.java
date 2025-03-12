dd3d29c73c39f7258a11988c18dbd754221c8286
MGJerry <kietthemouse@gmail.com>
2025-03-12 07:48:20 +0700

    switch from manual methods to lombok @Getter/@Setter

--- src/main/java/com/example/demo/model/AssessmentCategory.java
+++ src/main/java/com/example/demo/model/AssessmentCategory.java
@@ -1,7 +1,11 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "AssessmentCategories")
 public class AssessmentCategory {
@@ -13,44 +17,4 @@ public class AssessmentCategory {
     private String description;
     private Double weight;
     private String status;
-
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Double getWeight() {
-        return weight;
-    }
-
-    public void setWeight(Double weight) {
-        this.weight = weight;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
 }

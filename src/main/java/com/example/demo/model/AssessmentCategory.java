669575ab35392691194abc2fed020a9325c11457
MGJerry <kietthemouse@gmail.com>
2025-03-12 09:36:09 +0700

    Revert "switch from manual methods to lombok @Getter/@Setter"
    This reverts commit dd3d29c73c39f7258a11988c18dbd754221c8286.

--- src/main/java/com/example/demo/model/AssessmentCategory.java
+++ src/main/java/com/example/demo/model/AssessmentCategory.java
@@ -1,11 +1,7 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 @Entity
 @Table(name = "AssessmentCategories")
 public class AssessmentCategory {
@@ -17,4 +13,44 @@ public class AssessmentCategory {
     private String description;
     private Double weight;
     private String status;
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
--- src/main/java/com/example/demo/model/ERole.java
+++ src/main/java/com/example/demo/model/ERole.java
@@ -2,7 +2,6 @@ package com.example.demo.model;
 
 public enum ERole {
   ROLE_USER,
-  ROLE_MEMBER,
   ROLE_EXPERT,
   ROLE_ADMIN
 }

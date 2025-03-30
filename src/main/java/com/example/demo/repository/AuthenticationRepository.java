64d3c4599624fe564c48ec19838df3fe47dd4acf
cntduongg <cntduongg.9@gmail.com>
2025-03-31 03:56:41 +0700

    Add validation for register"

--- src/main/java/com/example/demo/repository/AuthenticationRepository.java
+++ src/main/java/com/example/demo/repository/AuthenticationRepository.java
@@ -11,4 +11,6 @@ public interface AuthenticationRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
     Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }

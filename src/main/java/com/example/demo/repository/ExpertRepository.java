870e1c629ea81cd02be4bda47dcdac2676125f82
cntduongg <cntduongg.9@gmail.com>
2025-04-05 19:37:25 +0700

    delete useless import

--- src/main/java/com/example/demo/repository/ExpertRepository.java
+++ src/main/java/com/example/demo/repository/ExpertRepository.java
@@ -2,7 +2,6 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Expert;
 import com.example.demo.enums.EStatus;
-import com.example.demo.model.Specialization;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,8 +11,4 @@ import java.util.Optional;
 @Repository
 public interface ExpertRepository extends JpaRepository<Expert, Long> {
     Optional<Expert> findById(Long id);
-
-    List<Expert> findBySpecialization(Specialization specialization);
-
-    List<Expert> findBySpecializationAndSpecializationLevelGreaterThanEqual(Specialization specialization, int specializationLevel);
 }
